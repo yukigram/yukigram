5c65049a28a758fd1990403e0b667671d136f204
Yuki Sireneva <yuki.utk8g@gmail.com>
2026-04-21 09:04:19 +0300

    v6.7.7

--- package.nix
+++ package.nix
@@ -6,9 +6,9 @@ telegram-desktop.override {
   pname = "yukigram";
   unwrapped = telegram-desktop.unwrapped.overrideAttrs (final: prev: {
     name = "yukigram-unwrapped";
-    version = "6.7.6";
+    version = "6.7.7";
     src = prev.src.overrideAttrs {
-      hash = "sha256-TGI1SLtzjjDaodQc+JIVRRiwCy9PCO3MuPfv2DpDFxo=";
+      hash = "sha256-uvVfzpb69tTHPZv1BICun6/Etumjdh3A8qLpMCAa/FM=";
     };
     patches = let
       readDir' = d: lib.pipe d [builtins.readDir builtins.attrNames (map (lib.path.append d))];
