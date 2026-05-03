a8755d9172bc7997c4fce4fec26b52d64e8bf591
Yuki Sireneva <sylfn@disroot.org>
2026-05-03 14:45:27 +0300

    format local.nix

--- local.nix
+++ local.nix
@@ -2,37 +2,40 @@ import ./. {
   appId = "io.github.yukigram.devel";
   customNixpakConfig = let
     path = toString ../yukigram-worktree/out/Debug;
-  in {pkgs, ...}: {
-    bubblewrap.bind.ro = [path];
-    app.package = with pkgs; lib.mkForce (buildFHSEnv {
-      name = "yukigram-dev-env";
-      targetPkgs = p: with p; [
-        libgcc
-        fontconfig
-        freetype
-        glib
-        libX11
-        wayland
-        mesa
-        gtk3
-        libxkbcommon
-        xkeyboard-config
-        libGL
-        webkitgtk_4_1
-        dbus
-        kdePackages.breeze-icons
-        (stdenv.mkDerivation {
-          name = "yukigram-data";
-          src = ../yukigram-worktree/app/share;
-          dontUnpack = true;
-          installPhase = ''
-            mkdir -p $out/share
-            cp -r $src/. $out/share/
-          '';
-        })
-      ];
-      executableName = "yukigram";
-      runScript = path + "/yukigram";
-    });
-  };
+  in
+    {pkgs, ...}: {
+      bubblewrap.bind.ro = [path];
+      app.package = with pkgs;
+        lib.mkForce (buildFHSEnv {
+          name = "yukigram-dev-env";
+          targetPkgs = p:
+            with p; [
+              libgcc
+              fontconfig
+              freetype
+              glib
+              libX11
+              wayland
+              mesa
+              gtk3
+              libxkbcommon
+              xkeyboard-config
+              libGL
+              webkitgtk_4_1
+              dbus
+              kdePackages.breeze-icons
+              (stdenv.mkDerivation {
+                name = "yukigram-data";
+                src = ../yukigram-worktree/app/share;
+                dontUnpack = true;
+                installPhase = ''
+                  mkdir -p $out/share
+                  cp -r $src/. $out/share/
+                '';
+              })
+            ];
+          executableName = "yukigram";
+          runScript = path + "/yukigram";
+        });
+    };
 }
