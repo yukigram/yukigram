a5416f21d74bf5e6ae2d6096d8cf3003901cec73
Yuki Sireneva <sylfn@disroot.org>
2026-05-22 16:14:22 +0300

    v6.8.3.2-beta

--- package.nix
+++ package.nix
@@ -8,7 +8,7 @@ telegram-desktop.overrideAttrs (final: prev: {
   pname = "yukigram";
   unwrapped = prev.unwrapped.overrideAttrs (final: prev: {
     pname = "yukigram-unwrapped";
-    version = "6.8.3.1";
+    version = "6.8.3.2-beta";
     src = prev.src.overrideAttrs {
       rev = "v6.8.3";
       hash = "sha256-01PByyAPWVz14IvwJP/qxc4fdiyH4EYLbIvpFY2GITU=";
