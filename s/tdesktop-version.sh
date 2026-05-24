52dbe00d1af1f55effb195e7fefab74222a363c6
Yuki Sireneva <sylfn@disroot.org>
2026-05-25 01:52:02 +0300

    tdesktop-version: convert variables to CamelCase

--- s/tdesktop-version.sh
+++ s/tdesktop-version.sh
@@ -7,9 +7,9 @@ warn() { printf "\033[33m[WARN]\033[0m: %s\n" "$@"; }
 die() { printf "\033[31m[ERROR]\033[0m: %s\n" "$@"; exit 1; }
 
 test $# -eq 1 || die "no version supplied"
-VERSION="$1"
-[[ $VERSION == v* ]] || die "versions should start with v"
-BARE_VERSION="${VERSION:1}"
+Version="$1"
+[[ $Version == v* ]] || die "versions should start with v"
+BareVersion="${Version:1}"
 
 # Sanity checks
 test -f package.nix
@@ -18,22 +18,22 @@ test -d ../tdesktop
 test -f ../tdesktop/run-in-docker.sh
 
 #### Update upstream versions for packages ####
-HASH=$(
+Hash=$(
     nix-prefetch-git \
         --url "https://github.com/telegramdesktop/tdesktop" \
-        --rev "$VERSION" \
+        --rev "$Version" \
         --fetch-submodules \
         --no-add-path \
     | jq .hash
 )
 
 sed -i -E -f - .github/workflows/bincache.yml <<EOF
-/ref:/s/: .*$/: $VERSION/
-/# Upstream version/s/=.* #/=$BARE_VERSION #/
+/ref:/s/: .*$/: $Version/
+/# Upstream version/s/=.* #/=$BareVersion #/
 EOF
 sed -i -E -f - package.nix <<EOF
-/rev/s/".*"/"$VERSION"/
-/hash/s|"sha256-.*"|$HASH|
+/rev/s/".*"/"$Version"/
+/hash/s|"sha256-.*"|$Hash|
 EOF
 git add .github/workflows/bincache.yml package.nix
 
@@ -44,14 +44,14 @@ info 'Flatpak permissions might have been changed.' \
 read -r _confirmation
 git add flatpak/io.github.yukigram.yml
 
-git commit -m "tdesktop/$VERSION: packaging"
+git commit -m "tdesktop/$Version: packaging"
 
 #### Update the patchset ####
 pushd ../tdesktop
 
 # Reset to the clean slate
 git fetch --all
-git reset --hard "$VERSION"
+git reset --hard "$Version"
 git submodule update --init --recursive
 ../yukigram/s/flatten.sh
 
@@ -71,13 +71,13 @@ info 'Telegram might have released a new `centos_env` image' \
     'Find the correct version from and enter it here' \
     'Versions: https://github.com/telegramdesktop/tdesktop/pkgs/container/tdesktop%2Fcentos_env/versions'
 while
-    read -r HASH;
-    [[ $HASH != sha256:???????????????????????????????????????????????????????????????? ]]
+    read -r Hash;
+    [[ $Hash != sha256:???????????????????????????????????????????????????????????????? ]]
 do
     warn 'This does not look like a hash to me. Try again'
 done
 sed -i -E -f - run-in-docker.sh <<EOF
-/centos_env/s/sha256:.* /$HASH /
+/centos_env/s/sha256:.* /$Hash /
 EOF
 git add run-in-docker.sh
 git commit --fixup 'HEAD^{/Yukigram) Build support}' --allow-empty
@@ -88,4 +88,4 @@ find ../yukigram/tdesktop/cur/ -name '*.patch' -delete
 ../yukigram/s/format-patch.sh
 popd
 git add tdesktop/cur/
-git commit -m "tdesktop/$VERSION: patches"
+git commit -m "tdesktop/$Version: patches"
