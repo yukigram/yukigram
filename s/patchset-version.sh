8ebaf043b4721c991c2636c3cac914d503e6f39d
Yuki Sireneva <sylfn@disroot.org>
2026-05-24 18:42:06 +0300

    Change wip changelog version in patchset-version

--- s/patchset-version.sh
+++ s/patchset-version.sh
@@ -9,6 +9,8 @@ VERSION="$1"
 BARE_VERSION="${VERSION:1}"
 [[ $VERSION == *-* ]] && DEVEL=true || DEVEL=false
 [[ $DEVEL == true ]] && APP_ID=io.github.yukigram.devel || APP_ID=io.github.yukigram
+BARE_VERSION_SPACED=${BARE_VERSION/-/ }
+VERSION_DATE=$(TZ=UTC date +%d.%m.%y)
 
 # Sanity checks
 test -f package.nix
@@ -27,15 +29,24 @@ s/io.github.yukigram(.devel)?/$APP_ID/
 EOF
 
 pushd ../tdesktop
+
 sed -i -E -f - Telegram/SourceFiles/boxes/about_box.cpp <<EOF
 /box->setTitle/s/: ".*"/: "$VERSION"/
 EOF
 git add Telegram/SourceFiles/boxes/about_box.cpp
 git commit --fixup 'HEAD^{/Yukigram) Version}' --allow-empty
+
+sed -i -E -f - changelog.txt <<EOF
+/^9999 wip/s/^.*$/$BARE_VERSION_SPACED ($VERSION_DATE)/
+EOF
+git add changelog.txt
+git commit --fixup 'HEAD^{/Yukigram) Changelog}' --allow-empty
+
 ../yukigram/s/rebase.sh
 ../yukigram/s/format-patch.sh
+
 popd
 
-git add package.nix flatpak/io.github.yukigram.yml tdesktop/cur/????-Yukigram-Version.patch
+git add package.nix flatpak/io.github.yukigram.yml tdesktop/cur/????-Yukigram-Version.patch tdesktop/cur/????-Yukigram-Changelog.patch
 git commit -m "$VERSION"
 git tag -m "$VERSION" "$VERSION"
