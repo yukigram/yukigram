9627ee6ffdb3d1989d8d202ddd9915eb76d776ed
Yuki Sireneva <sylfn@disroot.org>
2026-07-18 11:47:11 +0000

    v7.0.2.0

--- package.nix
+++ package.nix
@@ -10,13 +10,13 @@ telegram-desktop.overrideAttrs (final: prev: {
   pname = "yukigram";
   unwrapped = prev.unwrapped.overrideAttrs (final: prev: {
     pname = "yukigram-unwrapped";
-    version = "7.0.2.0-rc.1+wip";
+    version = "7.0.2.0";
     src = prev.src.overrideAttrs {
       rev = "v7.0.2";
       hash = "sha256-G/A5J2m1sXHD50zDmMD9ehnorAGRjnQ+YGMv6DEiJcQ=";
     };
     cmakeFlags = prev.cmakeFlags ++ [
-      (lib.cmakeBool "DEVEL" true)
+      (lib.cmakeBool "DEVEL" false)
     ];
     patches = let
       readDir' = d: lib.pipe d [builtins.readDir builtins.attrNames (map (lib.path.append d))];
@@ -26,7 +26,7 @@ telegram-desktop.overrideAttrs (final: prev: {
       description = "Telegram Desktop, minus the bullshit, plus the features";
       homepage = "https://github.com/yukigram/yukigram";
       changelog = "https://github.com/yukigram/yukigram/releases";
-      mainProgram = "io.github.yukigram.devel";
+      mainProgram = "io.github.yukigram";
     };
   });
 
