1012f2eb94af9ae14c07aee725c780a267c021d1
Yuki Sireneva <sylfn@disroot.org>
2026-07-18 08:01:11 +0000

    tdesktop/v7.0.2: packaging

--- package.nix
+++ package.nix
@@ -12,8 +12,8 @@ telegram-desktop.overrideAttrs (final: prev: {
     pname = "yukigram-unwrapped";
     version = "7.0.1.0+wip";
     src = prev.src.overrideAttrs {
-      rev = "v7.0.1";
-      hash = "sha256-zvJEfqMD4q2DI434dP6WfHU5TXAwpspdpgV959+A/0U=";
+      rev = "v7.0.2";
+      hash = "sha256-G/A5J2m1sXHD50zDmMD9ehnorAGRjnQ+YGMv6DEiJcQ=";
     };
     cmakeFlags = prev.cmakeFlags ++ [
       (lib.cmakeBool "DEVEL" true)
